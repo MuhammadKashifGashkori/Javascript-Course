21f33d6df1082a755cbe4ccf6fd8af345dcbf555
MuhammadKashifGashkori <12.bscs.23@gmail.com>
2026-08-05 17:06:17 +0500

    Lesson 02 - Introduction to Varibles and Constants

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("Kashif");
+let name="Kashif Gashkori";
+let age=35;
+console.log(name)
+console.log(age)
+age=35;
+console.log("Age after Change",age);
+const country="Pakistan";
+console.log("Country before change", country)
+country="UAE"
+console.log("Country after change",country)
